ac9cbaa1b85a32915db8f3a7d5fd83fd2f5ba4ed
Leonard Ehrenfried <mail@leonard.io>
2023-07-21 15:01:40 +0200

    Get complete list of tags to be deleted

--- repoprune/repoprune.py
+++ repoprune/repoprune.py
@@ -1,8 +1,11 @@
 import requests
 import os
+from _datetime import datetime, timezone
 from tabulate import tabulate
+from dateutil.parser import parse
 
 DOCKERHUB_BASE = "https://hub.docker.com"
+MAX_UNPULLED_DAYS= 180
 
 
 def headers(token):
@@ -10,6 +13,16 @@ def headers(token):
     "Authorization": f"JWT {token}"
   }
 
+def should_delete(last_pulled):
+    if last_pulled is not None:
+      time = parse(last_pulled)
+      today = datetime.now(timezone.utc)
+      duration = today - time
+      days = duration.days
+      return days > MAX_UNPULLED_DAYS
+    else:
+      # was never pulled
+      return True
 
 def get_token(user, password):
   print(f"Getting token for user '{user}'")
@@ -27,15 +40,25 @@ def get_token(user, password):
 if __name__ == "__main__":
     user = os.environ["CONTAINER_REGISTRY_USER"]
     pw = os.environ["CONTAINER_REGISTRY_PASSWORD"]
+    repo = "opentripplanner"
 
     token = get_token(user, pw)
 
-    url = f"{DOCKERHUB_BASE}/v2/repositories/{user}/opentripplanner/tags/?page_size=10000"
+    url = f"{DOCKERHUB_BASE}/v2/repositories/{user}/{repo}/tags/?page_size=10000"
 
     resp = requests.get(url, headers=headers(token))
 
     tags = resp.json()['results']
 
+    table = [["Tag", "Last pulled", "To be deleted"]]
     for tag in tags:
-        print(tag['name'])
-        print(tag['tag_last_pushed'])
+        last_pulled = tag['tag_last_pulled']
+
+        row = [tag['name'], last_pulled, should_delete(last_pulled)]
+        table.append(row)
+
+
+    print("")
+    print(f"Tags for repo {user}/{repo} on {DOCKERHUB_BASE}")
+    print("")
+    print(tabulate(table, headers="firstrow"))
